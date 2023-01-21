3d35aefe0bbf4a2c22097c9fb28613f0cb7db5ee
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-21 15:14:12 +0530

    Refactor: Adding New Contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,10 +1,33 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
     Contact contact = new Contact();
 
-    void contact(){
-
+    void addContact(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First name = ");
+        String firstName = scanner.next();
+        System.out.println("Enter Last name = ");
+        String lastName = scanner.next();
+        System.out.println("Enter Address = ");
+        String address = scanner.next();
+        System.out.println("Enter City = ");
+        String city = scanner.next();
+        System.out.println("Enter State = ");
+        String state = scanner.next();
+        System.out.println("Enter Phone Number = ");
+        String phoneNumber = scanner.next();
+        System.out.println("Enter Email = ");
+        String email= scanner.next();
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setPhoneNumber(phoneNumber);
+        contact.setEmail(email);
     }
 }
